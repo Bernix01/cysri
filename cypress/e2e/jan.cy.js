cc91723a079b5b6e4dcb9c056940b29eea64ff95
Bernix01 <gbernal096@gmail.com>
2024-01-16 21:36:38 -0500

    add all year support

--- cypress/e2e/jan.cy.js
+++ cypress/e2e/jan.cy.js
@@ -5,8 +5,6 @@ Cypress.on("uncaught:exception", (err, runnable) => {
 });
 
 const downloadsFolder = Cypress.config("downloadsFolder");
-// month is 1 based
-const month = "1";
 const year = "2023";
 // 0 is all - 1 is invoices - 2 is credit notes
 const docType = "1";
@@ -30,79 +28,97 @@ describe("invoice download xml", () => {
     );
   });
 
-  it("jan", async function () {
-    cy.get("#id-sri-splash", { timeout: 10000 }).should("not.exist");
-    cy.wait(3000 + Math.random() * 7000);
+  it("download all months", async function () {
+    cy.wrap(Array(12)).as("months");
+    cy.get("@months").each((month, monthIndex, $list) => {
+      cy.get("#id-sri-splash", { timeout: 10000 }).should("not.exist");
+      cy.wait(3000 + Math.random() * 7000);
 
-    // trying to create random scroll
-    cy.scrollTo("bottom", { ensureScrollable: false });
-    cy.wait(500 + Math.random() * 500);
-    cy.scrollTo("top", { ensureScrollable: false });
-    cy.wait(500 + Math.random() * 500);
+      // trying to create random scroll
+      cy.scrollTo("bottom", { ensureScrollable: false });
+      cy.wait(500 + Math.random() * 500);
+      cy.scrollTo("top", { ensureScrollable: false });
+      cy.wait(500 + Math.random() * 500);
 
-    cy.get("#frmPrincipal\\:ano").select(year);
-    cy.wait(200 + Math.random() * 700);
-    cy.get("#frmPrincipal\\:mes").select(month);
-    cy.wait(200 + Math.random() * 700);
-    cy.get("#frmPrincipal\\:dia").select("0"); // all days
-    cy.wait(200 + Math.random() * 700);
-    cy.get("#frmPrincipal\\:cmbTipoComprobante").select(docType);
-    cy.wait(200 + Math.random() * 700);
+      cy.get("#frmPrincipal\\:ano").select(year);
+      cy.wait(200 + Math.random() * 700);
+      cy.get("#frmPrincipal\\:mes").select(monthIndex + 1); // month is 1 based
+      cy.wait(200 + Math.random() * 700);
+      cy.get("#frmPrincipal\\:dia").select("0"); // all days
+      cy.wait(200 + Math.random() * 700);
+      cy.get("#frmPrincipal\\:cmbTipoComprobante").select(docType);
+      cy.wait(200 + Math.random() * 700);
 
-    cy.log("searching");
-    cy.get("#btnRecaptcha").click();
-    cy.log("waiting for captcha");
-    cy.get("#frmPrincipal\\:tablaCompRecibidos_paginator_bottom", {
-      timeout: 180000, // so you can complete captcha if asked to
-    }).should("be.visible");
-    cy.log("captcha passed");
+      cy.log("searching");
+      cy.get("#btnRecaptcha").click();
+      cy.log("waiting for captcha");
+      cy.get("#frmPrincipal\\:tablaCompRecibidos_paginator_bottom", {
+        timeout: 180000, // so you can complete captcha if asked to
+      }).should("be.visible");
+      cy.log("captcha passed");
 
-    cy.get(
-      "#frmPrincipal\\:tablaCompRecibidos_paginator_bottom > .ui-paginator-rpp-options"
-    ).select("75");
-    cy.wait(1000 + Math.random() * 1000);
-    cy.scrollTo("bottom", { ensureScrollable: false });
+      cy.get(
+        "#frmPrincipal\\:tablaCompRecibidos_paginator_bottom > .ui-paginator-rpp-options"
+      ).select("75");
+      cy.wait(1000 + Math.random() * 1000);
+      cy.scrollTo("bottom", { ensureScrollable: false });
 
-    cy.get(
-      "#frmPrincipal\\:tablaCompRecibidos_paginator_bottom > .ui-paginator-current"
-    ).then((value) => {
-      const availablePages = value.text();
-      // regex filter to get the numbers in "(1 of 1)"
-      const pages = availablePages.match(/\d+/g).map(Number);
-      cy.wrap(pages).as("pages");
-    });
-
-    cy.get("#frmPrincipal\\:tablaCompRecibidos_data")
-      .find("tr")
-      .each(($el, index, $list) => {
-        cy.exec(`[ -f ${downloadsFolder}/Factura-${year}-${month}-${index}.xml ] && echo "file exists"`, {failOnNonZeroExit: false}).then((stdout) => {
-          const exists = stdout.stdout === "file exists";
-          cy.wrap(exists).as(`fileExists${index}`);
-        });
-        // fileExists(
-        //   `${downloadsFolder}/Factura-${year}-${month}-${index}.xml`,
-        //   index
-        // );
-        cy.get(`@fileExists${index}`).then((exists) => {
-          if (!exists) {
-            cy.get(
-              `#frmPrincipal\\:tablaCompRecibidos\\:${index}\\:lnkXml`
-            ).click();
-            cy.readFile(`${downloadsFolder}/Factura.xml`).should("exist");
-            cy.fsRename({
-              newPath: `${downloadsFolder}/Factura-${year}-${month}-${index}.xml`,
-              path: `${downloadsFolder}/Factura.xml`,
-            });
-          }
-        });
+      cy.get(
+        "#frmPrincipal\\:tablaCompRecibidos_paginator_bottom > .ui-paginator-current"
+      ).then((value) => {
+        const availablePages = value.text();
+        // regex filter to get the numbers in "(1 of 1)"
+        const pages = availablePages.match(/\d+/g).map(Number);
+        cy.wrap(Array(pages[1])).as("pages");
       });
-    // });
-    // });
+
+      cy.get("@pages").each((page, pageIndex, $list) => {
+        // for each available page
+        cy.log(`page ${pageIndex} of ${$list.length - 1}`);
+        cy.get("#frmPrincipal\\:tablaCompRecibidos_data")
+          .find("tr")
+          .each(($el, index, $list) => {
+            // get invoice number
+            cy.get(`tr:nth-child(${index + 1}) > td:nth-child(3) > div`).then(
+              ($el) => {
+                const text = $el.text().replace(" ", "_");
+                cy.wrap(text).as(`invoiceNumber`);
+              }
+            );
+
+            cy.get("@invoiceNumber").then((invoiceNumber) => {
+              // check if file exists
+              cy.exec(
+                `[ -f ${downloadsFolder}/${invoiceNumber}.xml ] && echo "file exists"`,
+                { failOnNonZeroExit: false }
+              ).then((stdout) => {
+                const exists = stdout.stdout === "file exists";
+                cy.wrap(exists).as(`fileExists`);
+              });
+
+              //process row
+              cy.get(`@fileExists`).then((exists) => {
+                if (!exists) {
+                  cy.get(
+                    `#frmPrincipal\\:tablaCompRecibidos\\:${index}\\:lnkXml`
+                  ).click();
+                  cy.readFile(`${downloadsFolder}/Factura.xml`).should("exist");
+                  cy.fsRename({
+                    newPath: `${downloadsFolder}/${invoiceNumber}.xml`,
+                    path: `${downloadsFolder}/Factura.xml`,
+                  });
+                }
+              });
+            });
+          });
+
+        // go to next page, if available
+        if ($list.length - 1 !== pageIndex) {
+          cy.get(
+            "#frmPrincipal\\:tablaCompRecibidos_paginator_bottom > .ui-paginator-next"
+          ).click();
+        }
+      });
+    });
   });
 });
-
-function fileExists(filePath, index) {
-  cy.fsFileExists(filePath).then((exists) => {
-    cy.wrap(exists).as(`fileExists${index}`);
-  });
-}
